daaafd2971b7f2e61ab32bcc8295ed76a18488be
Glally <lallyGus@gmail.com>
2017-02-20 12:48:26 -0800

    Plugin now mobile friendly

--- video_link.php
+++ video_link.php
@@ -9,10 +9,19 @@ Author URI: https://ca.linkedin.com/in/augustine-lally-34691a27
 */
 
 
-    
+add_action( 'wp_enqueue_scripts', 'video_style' );
 add_filter( 'mce_external_plugins', 'get_js' );
 // Add the button to the post editor
 add_filter( 'mce_buttons', 'get_button' );
+
+
+// Get the css to makesure it can work on most screens
+function get_css() {
+
+wp_register_style( 'iframe.embed', plugins_url('/css/video_size.css', __FILE__) );
+wp_enqueue_style( 'iframe.embed' );
+}
+add_action('wp_enqueue_scripts','get_css');
 
 // Get the javascript file so the button can work
 function get_js( $js ) {
